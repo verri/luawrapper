75f540b707ec1b1ed2afa07b96ebae72868cbe37
Filipe Verri <filipeverri@gmail.com>
2017-10-07 09:55:17 -0300

    Fixed map test cases.

--- tests/maps.cpp
+++ tests/maps.cpp
@@ -1,6 +1,8 @@
 #include <LuaContext.hpp>
 #include <gtest/gtest.h>
 
+#include <boost/functional/hash.hpp>
+
 TEST(Maps, MapString) {
     LuaContext context;
 
@@ -22,7 +24,7 @@ TEST(Maps, MapStringReverse) {
     map_type a{{"foo", 1}, {"bar", 2}};
     context.writeVariable("a", a);
 
-    const auto b = context.readVariable<std::map<std::string, int>>("a");
+    const auto b = context.readVariable<map_type>("a");
     EXPECT_EQ(1, b.at("foo"));
     EXPECT_EQ(2, b.at("bar"));
 }
@@ -46,6 +48,26 @@ TEST(Maps, MapVariant) {
     // if `std::string` would come first, any `int` key would be converted
     // to `std::string` when reading.
     using map_type = std::map<boost::variant<int, std::string>, int>;
+
+    map_type a{{"foo", 1}, {2, 3}};
+    EXPECT_EQ(1, a.at("foo"));
+    EXPECT_EQ(3, a.at(2));
+
+    context.writeVariable("a", a);
+    EXPECT_EQ(1, context.readVariable<int>("a", "foo"));
+    EXPECT_EQ(3, context.readVariable<int>("a", 2));
+
+    const auto b = context.readVariable<map_type>("a");
+    EXPECT_EQ(1, b.at("foo"));
+    EXPECT_EQ(3, b.at(2));
+}
+
+TEST(Maps, UnorderedMapVariant) {
+    LuaContext context;
+
+    // if `std::string` would come first, any `int` key would be converted
+    // to `std::string` when reading.
+    using map_type = std::unordered_map<boost::variant<int, std::string>, int, boost::hash<boost::variant<int, std::string>>>;
 
     map_type a{{"foo", 1}, {2, 3}};
     EXPECT_EQ(1, a.at("foo"));
